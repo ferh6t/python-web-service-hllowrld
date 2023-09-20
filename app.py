58d2e203140dc75ceac6fb6c1e2f65a0c2745e2d
ferh6t <karacaferhat6@gmail.com>
2023-09-20 01:46:22 -0700

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route and a view function
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+# Run the app if this script is executed
+if __name__ == '__main__':
+    custom_port = 8080
+    app.run(port=custom_port)
